a6bf14bd320695bee01b86d7dc82f383ad861adf
Rahul kapoor <kapoorrahul304@gmail.com>
2023-05-08 14:21:36 +0530

    assign complete

---
+++ block-BNaacf/app/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/app",(err)=>{
+    console.log(err ? err : "connected with server");
+})
+var app = express();
+
+
+app.use((req,res,next)=>{
+    next();
+})
+
+app.get("/",(req,res)=>{
+    res.send('Welcome');
+})
+
+app.listen(3000,()=>{
+    console.log('app listen on port 3k');
+})
